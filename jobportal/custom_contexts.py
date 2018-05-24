2927a6b12de063f9906a667f29df6174f7eca9cf
Prachi <prachi@LYS.prachi-lys>
2018-05-24 11:09:12 +0530

    created jobportal project directory

---
+++ jobportal/custom_contexts.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+def custom_context(request):
+    """The main context for the basic values needed for the project."""
+
+    context = {
+        'app_title': 'JOB Portal',
+    }
+    return context
